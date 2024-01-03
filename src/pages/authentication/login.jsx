96b4851af2f8038678edb196a03eaef848f8c11b
datzxje <tudabest12@gmail.com>
2024-01-03 22:37:05 +0700

    feature/login

--- src/pages/authentication/login.jsx
+++ src/pages/authentication/login.jsx
@@ -1,53 +1,84 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-const Login = () => {
+export default function SignIn() {
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const nagivate = useNavigate();
 
-  const handleLogin = () => {
-    // You can implement your authentication logic here
-    // For simplicity, I'm checking if both username and password are non-empty
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const username = formData.get('username');
+    const password = formData.get('password');
+  
     if (username && password) {
       // If authentication is successful, navigate to another page
-      nagivate('/dashboard');
+      navigate('/dashboard');
     } else {
       setError('Invalid username or password');
     }
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <TextField
-          label="Username"
-          variant="outlined"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          style={{ marginBottom: '20px' }}
-        />
-        <br />
-        <TextField
-          label="Password"
-          variant="outlined"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{ marginBottom: '20px' }}
-        />
-        <br />
-        <Button variant="contained" onClick={handleLogin}>
-          Login
-        </Button>
-        {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
-      </form>
-    </div>
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5" sx={{ fontWeight: 'bold', color: '#365486'}}>
+          Sign in
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button 
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: '#365486' }}
+          >
+            Sign In
+          </Button>
+          {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
+        </form>
+      </Box>
+    </Container>
   );
-};
-
-export default Login;
+}
