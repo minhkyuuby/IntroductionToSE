0d43e70d1812442caca0490f1ecf9bcb75e0c6e1
dinhkhoadev <dinhkhoa215403@gmail.com>
2024-01-04 15:17:28 +0700

    update vehicle resident service

--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
-import { Container, Button } from '@mui/material';
+import { Container, Button,Typography, Box } from '@mui/material';
 import ChangeVehicleModal from '../components/Vehicle.jsx/ChangeVehicleModal';
 import VehicleTable from '../components/Vehicle.jsx/VehicleTable';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AddIcon from '@mui/icons-material/Add';
 const initialRows = [
   // Initial data for the table
 ];
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#7FC7D9',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 export default function Vehicles() {
   const [openModal, setOpenModal] = useState(false);
@@ -25,14 +38,33 @@ export default function Vehicles() {
     setRows([...rows, newVehicle]);
   };
   return (
-    <Container>
-      <div >
-        Bảng xe 
-      </div>
-      <div>
-        <Button onClick={handleOpenModal} variant="outlined" size="small">Thêm xe mới </Button>
-      </div>
+    <Container component= "main" sx={{width: 1000}}>
+      <Typography component="h1" variant="h6" 
+        sx={{
+          backgroundColor: '#DCF2F1',
+          height: 40,
+          display: 'flex',
+          alignItems: 'center',
+          paddingLeft: 4,
+          boxShadow: 3,
+          marginTop: 5,
+          marginBottom: 2,
+          }}>
+          Danh sách cư dân 
+        </Typography>
+        <Box 
+          sx={{ 
+            display: 'flex', 
+            alignItems: 'center', 
+            justifyContent: 'space-between',
+            marginBottom: 2,
+            marginRight: 10
+          }}>
+      <ThemeProvider theme={theme}>
+        <Button onClick={handleOpenModal} variant="contained" color="primary" startIcon={<AddIcon />}>Thêm xe mới </Button>
+        </ThemeProvider>
       <ChangeVehicleModal open={openModal} handleClose={handleCloseModal} handleChangeVehicle={handleChangeVehicle}/>
+      </Box>
       <hr />
       <VehicleTable rows={rows} />
     </Container>
