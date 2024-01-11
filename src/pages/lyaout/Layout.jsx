5f9ed8dd45df8ec70fbce39a9421df8036b73901
vanminh <minhkyuuby@gmail.com>
2024-01-12 06:14:52 +0700

    modify dashboard

--- src/pages/lyaout/Layout.jsx
+++ src/pages/lyaout/Layout.jsx
@@ -6,7 +6,6 @@ export default function Layout({ children, page = "Dashboard" }) {
   return (
     <div>
       <Sidebar highlightedItem={page}/>
-
       {/* display the child prop */}
       {children}
     </div>
